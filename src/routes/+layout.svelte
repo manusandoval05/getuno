245e06b3e1895f83c2fd3fc5156b2eee946b6c76
Manuel Sandoval <manusandoval05@gmail.com>
2023-07-17 18:01:53 -0600

    Fixed the base prefix for the manifest.json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import 'bulma/css/bulma.min.css';
     import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
+    import { base } from '$app/paths';
 </script>
 
+<svelte:head>
+    <link rel="manifest" crossorigin="use-credentials" href="{base}/manifest.json" />
+</svelte:head>
 <slot></slot>
